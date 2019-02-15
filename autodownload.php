344f414b6448750a54c077d47bacc065f88e167e
JuLiaN47Vbitbucket <julianvolland749@gmail.com>
2019-02-15 15:23:30 +0100

    Installation angepasst und Parameter hinzugefüt

--- autodownload.php
+++ autodownload.php
@@ -16,6 +16,8 @@
 <img src="Steam.jpg" width="20" height="17" > <br>
 <input type="checkbox" name="Discord" value="1" class="checkbox" >  Discord
 <img src="Discord.jpg" width="20" height="17" > <br>
+<input type="checkbox" name="Chrome" value="1" class="checkbox" >  Chrome
+<img src="Chrome.jpg" width="20" height="17" > <br>
 
 
 
@@ -31,14 +33,16 @@
 $All = "@\"%SystemRoot%\System32\WindowsPowerShell\\v1.0\powershell.exe\" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \"iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))\" && SET \"PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin\"";
 
 if ($_POST['WinRar'] == '1') {
-	$WinRar = "Install-Package Winrar -provider Chocolatey -Force";
-	}
+	$WinRar = "Choco install WinRar -y";}
 
 if ($_POST['Steam'] == '1') {
-	$Steam = "Install-Package Steam -provider Chocolatey -Force";}
+	$Steam = "choco install Steam -y";}
 
 if ($_POST['Discord'] == '1') {
-    $Discord = "Install-Package Discord -provider Chocolatey -Force";}
+    $Discord = "choco install Discord -y";}
+
+if ($_POST['Chrome'] == '1') {
+    $Chrome = "choco install Chrome -y";}
 
 
 $file = fopen("autoinstall.bat","w");
@@ -46,6 +50,7 @@ fwrite($file,"$All
 $WinRar
 $Steam
 $Discord
+$Chrome
 
 
 
@@ -60,17 +65,7 @@ fclose($file);
 
 
 
-
-
-
-
-
-
-
-		
-		
-		
-		
+	
 		
 		
 		
