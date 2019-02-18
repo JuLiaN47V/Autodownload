08afccd8b463f00f9f776c4afcceb935ec754998
JuLiaN47Vbitbucket <julianvolland749@gmail.com>
2019-02-18 13:35:05 +0100

    +Java, Notepad++

--- autodownload.php
+++ autodownload.php
@@ -18,6 +18,13 @@
 <img src="Discord.jpg" width="20" height="17" > <br>
 <input type="checkbox" name="Chrome" value="1" class="checkbox" >  Chrome
 <img src="Chrome.jpg" width="20" height="17" > <br>
+<input type="checkbox" name="Notepad" value="1" class="checkbox" >  Notepad ++
+<img src="Notepad.jpg" width="20" height="17" > <br>
+<input type="checkbox" name="Java" value="1" class="checkbox" >  Java
+<img src="Java.jpg" width="20" height="17" > <br>
+
+
+
 <br>
 <input type="submit" action="Download.php">
 </form>
@@ -32,16 +39,22 @@
 $All = "@\"%SystemRoot%\System32\WindowsPowerShell\\v1.0\powershell.exe\" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))\" && SET \"PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin\"";
 
 if ($_POST['WinRar'] == '1') {
-	$WinRar = "choco install WinRar -y -Force";}
+	$WinRar = "choco install winRar -y --Force";}
 
 if ($_POST['Steam'] == '1') {
-	$Steam = "choco install Steam -y -Force";}
+	$Steam = "choco install steam -y --Force";}
 
 if ($_POST['Discord'] == '1') {
-    $Discord = "choco install Discord -y -Force";}
+    $Discord = "choco install discord -y --Force";}
 
 if ($_POST['Chrome'] == '1') {
-    $Chrome = "choco install Chrome -y -Force";}
+    $Chrome = "choco install googlechrome -y --Force";}
+	
+if ($_POST['Notepad'] == '1') {
+    $Notepad = "choco install notepadplusplus -y --Force";}
+	
+if ($_POST['Java'] == '1') {
+    $Java = "choco install jre8 -y --Force";}
 
 
 $file = fopen("autoinstall.bat","w");
@@ -50,6 +63,8 @@ $WinRar
 $Steam
 $Discord
 $Chrome
+$Notepad
+$Java
 
 
 
