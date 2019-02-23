84d99184411c665addd10ccd11ce451395f24092
JuLiaN47Vbitbucket <julianvolland749@gmail.com>
2019-02-23 10:28:22 +0100

    PHP-Teil von Index.php in Download.php getan

--- Download.php
+++ Download.php
@@ -1,8 +1,51 @@
 <?php
 
+$All = "@\"%SystemRoot%\System32\WindowsPowerShell\\v1.0\powershell.exe\" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))\" && SET \"PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin\"";
+
+if ($_POST['WinRar'] == '1') {
+	$WinRar = "choco install winRar -y --Force";}
+
+if ($_POST['Steam'] == '1') {
+	$Steam = "choco install steam -y --Force";}
+
+if ($_POST['Discord'] == '1') {
+    $Discord = "choco install discord -y --Force";}
+
+if ($_POST['Chrome'] == '1') {
+    $Chrome = "choco install googlechrome -y --Force";}
+	
+if ($_POST['Notepad'] == '1') {
+    $Notepad = "choco install notepadplusplus -y --Force";}
+	
+if ($_POST['Java'] == '1') {
+    $Java = "choco install jre8 -y --Force";}
+
+
+$file = fopen("autoinstall.bat","w");
+fwrite($file,"$All
+$WinRar
+$Steam
+$Discord
+$Chrome
+$Notepad
+$Java
+
+
+
+
+
+
+
+");
+fclose($file);
+
 header("Content-disposition: attachment; filename=autoinstall.bat");
 header("Content-type: application/bat");
 readfile("autoinstall.bat");
 exit;
 
+
+
+
 ?>
+
